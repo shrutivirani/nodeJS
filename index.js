bc31b14c57942f6b2d76d51e98c79de4e60d083b
shruti <dev014.rejoice@gmail.com>
2019-08-06 18:15:37 +0530

    node http request/response

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-var react = require ("./rectangle")
- function reactFunction(a,b){
-     console.log("We will count add and mul operation soon for a = " + a + " and b = "+ b)
-     react(a,b, (err, rectangle) =>{
-         if(err){
-             console.log("Error "+err)
-         }
-         else{
-             console.log("Addition of two number is "+ rectangle.add())
-             console.log("Multiplication of two numbers is "+ rectangle.mul())
-         }
-     })
-     console.log("will load after 2 sec interval")
- }
- reactFunction(101,10);
+// var react = require ("./rectangle")
+//  function reactFunction(a,b){
+//      console.log("We will count add and mul operation soon for a = " + a + " and b = "+ b)
+//      react(a,b, (err, rectangle) =>{
+//          if(err){
+//              console.log("Error "+err)
+//          }
+//          else{
+//              console.log("Addition of two number is "+ rectangle.add())
+//              console.log("Multiplication of two numbers is "+ rectangle.mul())
+//          }
+//      })
+//      console.log("will load after 2 sec interval")
+//  }
+//  reactFunction(101,10);
+
+const http = require('http');
+const hostname = 'localhost';
+const port = '3000';
+
+const server = http.createServer((req, res) =>{
+    console.log(req.headers);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>hello nodeJS</h1></body></html>');
+})
+server.listen(port, hostname, () => {
+    console.log(`server is running on http://${hostname}:${port}`);
+});
--- rectangle.js
+++ rectangle.js
@@ -1,18 +1,18 @@
-module.exports = (x, y, callback) => {
-    if(x <= 0 || y <= 0){
-        setTimeout(() =>
-        callback(
-        new Error("X and Y must more than 0 value")
-        ,null)
-        ,2000)
-    }
-    else{
-        setTimeout(() =>
-        callback(null,
-            {
-                add: () => (2+(x+y)),
-                mul:() => (2*(x+y))
-            })
-        ,2000)
-    }
-}
+// module.exports = (x, y, callback) => {
+//     if(x <= 0 || y <= 0){
+//         setTimeout(() =>
+//         callback(
+//         new Error("X and Y must more than 0 value")
+//         ,null)
+//         ,2000)
+//     }
+//     else{
+//         setTimeout(() =>
+//         callback(null,
+//             {
+//                 add: () => (2+(x+y)),
+//                 mul:() => (2*(x+y))
+//             })
+//         ,2000)
+//     }
+// }
